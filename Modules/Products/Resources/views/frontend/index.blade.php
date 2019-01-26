44f7e2883d6af8c155c05060e3de389872e2faa5
TranQuangHoai7292 <hoaitranquang7292@gmail.com>
2019-01-26 10:30:53 +0700

    fix view

--- Modules/Products/Resources/views/frontend/index.blade.php
+++ Modules/Products/Resources/views/frontend/index.blade.php
@@ -4,9 +4,9 @@
     <section class="banner-area organic-breadcrumb">
         <div class="container">
             <div class="breadcrumb-banner d-flex flex-wrap align-items-center">
-                <div class="col-first">
+                
                     <h1>Category {{$slug->name}}</h1>
-                </div>
+                
             </div>
         </div>
     </section>
